06816eaf19179470665ec992819c13ee3e0d115d
Nicolaj Blach Jensen <35289468+nico188f@users.noreply.github.com>
2023-11-01 12:15:23 +0100

    21.MergeTwoSortedLists

---
+++ 21MergeTwoSortedLists/21.MergeTwoSortedLists.c
@@ -0,0 +1,25 @@
+
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+    if (list1 == NULL) {
+        return list2;
+    }
+    if (list2 == NULL) {
+        return list1;
+    }
+
+    if (list1->val < list2->val) {
+        list1->next = mergeTwoLists(list1->next, list2);
+        return list1;
+    }
+    else {
+        list2->next = mergeTwoLists(list2->next, list1);
+        return list2;
+    }
+}
