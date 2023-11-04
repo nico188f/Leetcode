0bc5be734bf106e5e070b0808a0bc1f84fa36619
Nicolaj Blach Jensen <35289468+nico188f@users.noreply.github.com>
2023-11-04 12:36:27 +0100

    217. Contains Duplicate 2

--- 217. Contains Duplicate/index.js
+++ 217. Contains Duplicate/index.js
@@ -1,3 +1,5 @@
+//Solution 1
+
 var containsDuplicate = function (nums) {
     let map = new Map();
     for (let num of nums) {
@@ -7,4 +9,10 @@ var containsDuplicate = function (nums) {
         map.set(num, true);
     }
     return false;
+};
+
+//Solution 2
+var containsDuplicate = function (nums) {
+    const s = new Set(nums);
+    return s.size !== nums.length;
 };
